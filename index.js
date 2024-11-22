39384a5f7c2ffabc04b1121059fe05f656aca74a
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-11-22 22:29:59 +0530

    feat: react app server on / route

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(
+  "/",
+  express.static(path.join(__dirname, "/frontend/react-app-integartion/dist"))
+);
 
 const port = 3000;
 
