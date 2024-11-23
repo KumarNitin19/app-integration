00e6249fa86aafc950c8534a52d74a49a19da6f3
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-11-23 21:54:16 +0530

    feat: react reverse proxy setup done

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
-const path = require("path");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.json({ message: "Express app" });
 });
+
+app.use(
+  "/react",
+  createProxyMiddleware({
+    target: "http://localhost:5173",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/react": "",
+    },
+    onError: (err, req, res) => {
+      console.error("React Proxy Error:", err);
+      res.status(500).send("React Proxy Error");
+    },
+  })
+);
 
 const port = 3000;
 
