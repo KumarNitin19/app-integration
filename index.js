95c5f37c2bcaf51063e035bb6161dce5571cdd1c
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-11-23 21:45:27 +0530

    fix: few fixes

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const path = require("path");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
 
 const app = express();
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
 
 app.use(
   "/",
   express.static(path.join(__dirname, "/frontend/react-app-integartion/dist"))
 );
+
+nextApp.prepare().then(() => {
+  app.all("*", (req, res) => {
+    return handle(req, res);
+  });
+});
 
 const port = 3000;
 
