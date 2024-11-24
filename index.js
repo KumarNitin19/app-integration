33a565c1bf362ebde0db8796d3cdf17cf76ec27f
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-11-24 22:05:12 +0530

    fix: some app configuration

--- index.js
+++ index.js
@@ -7,69 +7,31 @@ app.get("/", (req, res) => {
   res.json({ message: "Express app" });
 });
 
-const reactProxyConfig = {
+const reactProxyConfig = createProxyMiddleware({
   target: "http://localhost:5173",
   changeOrigin: true,
   ws: true,
   secure: false,
-  headers: {
-    Connection: "keep-alive",
-  },
-  router: {
-    "/": "http://localhost:5173/",
+  followRefirects: true,
+  pathRewrite: (path, req) => {
+    console.log(path, "2");
+    return path;
   },
   onProxyReq: (proxyReq, req, res) => {
+    proxyReq.removeHeader("x-forwarded-host");
+    proxyReq.removeHeader("x-forwarded-proto");
     console.log("React Proxy Request:", req.method, req.url);
   },
   onError: (err, req, res) => {
     console.error("React Proxy Error:", err);
     res.status(500).send("React Proxy Error");
   },
-};
+});
 
-const reactPaths = [
-  "/react",
-  "/@fs/",
-  "/@vite/",
-  "/@id/",
-  "/node_modules/",
-  "/src/",
-  "/.vite/",
-  "/assets/",
-  ".hot-update.json",
-  ".hot-update.js",
-  ".js",
-  ".css",
-  ".jsx",
-  ".tsx",
-  ".ts",
-];
-app.use(reactPaths, createProxyMiddleware(reactProxyConfig));
-
-// app.use(
-//   "/next",
-//   createProxyMiddleware({
-//     target: "http://localhost:3001",
-//     changeOrigin: true,
-//     ws: true,
-//     router: {
-//       "/next": "http://localhost:3001/",
-//     },
-//     pathRewrite: function (path, req) {
-//       if (path.startsWith("/next/")) {
-//         return path.replace("/next", "");
-//       }
-//       return path;
-//     },
-//     onProxyReq: (proxyReq, req, res) => {
-//       console.log("React Proxy Request:", req.method, req.url);
-//     },
-//     onError: (err, req, res) => {
-//       console.error("React Proxy Error:", err);
-//       res.status(500).send("React Proxy Error");
-//     },
-//   })
-// );
+app.use(
+  ["/react", "/@vite/*", "/src/*", "/@reat-refresh", "/node_modules/*"],
+  reactProxyConfig
+);
 
 const port = 3000;
 
