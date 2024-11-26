42124258d5f6ccebcf00354e4e153376ecf64fad
anurag-elevano <nitinkumardeshmukh19@gmail.com>
2024-11-26 22:48:39 +0530

    feat: add cookie function added

--- frontend/react-app-integartion/vite.config.ts
+++ frontend/react-app-integartion/vite.config.ts
@@ -4,15 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    cors: true,
-    strictPort: true,
-    // origin: "http://localhost:5173",
-    hmr: {
-      clientPort: 3000,
-      host: "localhost",
-    },
-  },
-  base: "/",
 });
